569392c9a666790a67b3365ff23530baf39f095e
Elias De Windt <edewindt27@outlook.com>
2023-05-25 11:44:50 -0700

    successfully connected to websocket

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    const socket = new WebSocket("ws://127.0.0.1:8080/ws")
+    socket.onopen = () =>{
+        console.log("CONNECTED!!!")
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
