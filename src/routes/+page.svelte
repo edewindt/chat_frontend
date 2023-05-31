3c63573e83611bad425e2eaba9203876f37cd532
Elias De Windt <edewindt27@outlook.com>
2023-05-31 12:32:33 -0700

    handling websocket actions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,25 @@
     socket.onopen = () =>{
         console.log("CONNECTED!!!")
     }
+    socket.onclose = () =>{
+        console.log("CONNECTION CLOSED!!")
+    }
+    socket.onerror = () =>{
+        console.log("YOU SCREWED UP!!")
+    }
+    socket.onmessage = msg =>{
+        console.log(msg);
+    }
 </script>
+
+
+<div class="form-group">
+    <label for="text">Username:</label>
+    <input type="text">
+    <label for="text">Message:</label>
+    <input type="text">
+</div>
+<div class="output"></div>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
